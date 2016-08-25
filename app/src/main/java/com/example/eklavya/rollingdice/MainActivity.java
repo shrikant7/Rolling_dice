f9427bc3a3a1dc8bd53e216ca9136752925daec1
eklavya <shrikantlnmiit@gmail.com>
2016-08-26 01:04:58 +0530

    add landscape mode and show winner with score.

--- app/src/main/java/com/example/eklavya/rollingdice/MainActivity.java
+++ app/src/main/java/com/example/eklavya/rollingdice/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.eklavya.rollingdice;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mediaPlayer = null;
     int fs1=0,fs2=0,ts=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,13 @@ public class MainActivity extends AppCompatActivity {
         final TextView playerTurn = (TextView) findViewById(R.id.turn);
         final TextView turnScore = (TextView) findViewById(R.id.turn_score);
         final ImageView dice = (ImageView) findViewById(R.id.dice);
+        final TextView player1 = (TextView) findViewById(R.id.player1);
+        final TextView player2 = (TextView) findViewById(R.id.player2);
+
         Button roll = (Button) findViewById(R.id.roll_button);
         Button hold = (Button) findViewById(R.id.hold_button);
         Button reset = (Button) findViewById(R.id.reset_button);
-
+        mediaPlayer = MediaPlayer.create(this,R.raw.cheering);
         final Random randomGenerator = new Random();
 
         roll.setOnClickListener(new View.OnClickListener() {
@@ -52,19 +58,44 @@ public class MainActivity extends AppCompatActivity {
                 changePlayer(playerTurn);
                 ts=0;
                 turnScore.setText(ts+"");
+
+                if(fs1>=50)
+                {
+                    mediaPlayer.start();
+                    Toast toast=Toast.makeText(MainActivity.this,player1.getText()+" wins!!!\nscore:"+fs1, Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER,0,0);
+                    toast.show();
+                    updateToReset(score1, score2, turnScore, playerTurn, dice);
+                }
+                if(fs2>=50)
+                {
+                    mediaPlayer.start();
+                    Toast toast=Toast.makeText(MainActivity.this,player2.getText()+" wins!!!\nscore:"+fs2, Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER,0,0);
+                    toast.show();
+                    updateToReset(score1, score2, turnScore, playerTurn, dice);
+                }
+
             }
         });
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ts=0;fs1=0;fs2=0;
-                score1.setText(0+"");
-                score2.setText(0+"");
-                turnScore.setText(0+"");
-                playerTurn.setText(1+"");
+                updateToReset(score1, score2, turnScore, playerTurn, dice);
             }
         });
 
+    }
+
+    private void updateToReset(TextView score1, TextView score2, TextView turnScore, TextView playerTurn, ImageView dice) {
+        ts=0;
+        fs1=0;
+        fs2=0;
+        score1.setText(0+"");
+        score2.setText(0+"");
+        turnScore.setText(0+"");
+        playerTurn.setText(1+"");
+        dice.setImageResource(R.drawable.dice1);
     }
 
     private void changePlayer(TextView playerTurn) {
